e807d84e1bbcecdc117134ef19a025c7b023438d
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-12-07 12:03:39 +0100

    work on styles and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { InputField } from "./components/InputField/InputField";
+import { Todo } from "./types/model";
 
 // React.ReactNode - this support all of the types
 
 const App: React.FC = () => {
+  const [todoText, setTodo] = useState<string>("");
+  const [todos, settodos] = useState<Todo[]>([]);
+
+  console.log(todoText);
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField/>
+      <InputField todoText={todoText} setTodo={setTodo} />
     </div>
   );
 };
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,10 +1,24 @@
 import React from "react";
-import '../../components/styles.css'
+import "../../components/styles.css";
 
-export const InputField = () => {
+interface InputFieldProps {
+  todoText: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const InputField: React.FC<InputFieldProps> = ({
+  todoText,
+  setTodo,
+}) => {
   return (
     <form className="input">
-      <input type="input" placeholder="enter task" className="input__box" />
+      <input
+        value={todoText}
+        onChange={(e) => setTodo(e.target.value)}
+        type="input"
+        placeholder="enter task"
+        className="input__box"
+      />
       <button className="input__submit" type="submit">
         Go
       </button>
