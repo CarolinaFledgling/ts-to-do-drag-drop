38bead5a4c5360601163ea0deed0777cc741c0cd
Karolina Kulinska <karolina.kulinska89@gmail.com>
2022-12-14 21:56:01 +0100

    added functionality for editing tasks

--- src/components/SingleToDo/SingleTodo.tsx
+++ src/components/SingleToDo/SingleTodo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../components/styles.css";
 import { Todo } from "../../types/model";
 import { AiFillEdit } from "react-icons/ai";
@@ -24,6 +24,11 @@ export const SingleTodo: React.FC<SingleTodoProps> = ({
   todos,
   setTodos,
 }) => {
+  // Editing functionality with two states. First one will keep the track if the Edit Mode is on or not.
+  const [isEdit, setIsEdit] = useState<boolean>(false);
+  // Second one will keep the value of editing input.
+  const [editTodo, setEditTodo] = useState<string>(todo.todoValue);
+
   const handleDone = (id: number) => {
     setTodos(
       todos.map((todo) =>
@@ -36,16 +41,41 @@ export const SingleTodo: React.FC<SingleTodoProps> = ({
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todoValue: editTodo } : todo))
+    );
+
+    setIsEdit(false);
+  };
+
   return (
-    <form className="todos__single">
-      {todo.isDone ? (
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {isEdit ? (
+        <input
+          className="todos-single--text"
+          value={editTodo}
+          onChange={(e) => {
+            setEditTodo(e.target.value);
+          }}
+        />
+      ) : todo.isDone ? (
         <s className="todos__single--text">{todo.todoValue}</s>
       ) : (
         <span className="todos__single--text">{todo.todoValue}</span>
       )}
 
       <div>
-        <span className="icon">
+        <span
+          className="icon"
+          onClick={() => {
+            if (!isEdit && !todo.isDone) {
+              setIsEdit(!isEdit);
+            }
+          }}
+        >
           <AiFillEdit />
         </span>
         <span className="icon">
